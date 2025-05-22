9e62dad9c33f645615721bdbe910aea04cdc2701
kingkami0 <disrael1@stu.vistula.edu.pl>
2025-05-22 18:47:57 +0200

    Update hellocontroller.java

--- first-project-java-spring/src/main/java/pl/edu/vistula/first_project_java_spring/controller/hellocontroller.java
+++ first-project-java-spring/src/main/java/pl/edu/vistula/first_project_java_spring/controller/hellocontroller.java
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
         public String hello() { return "Hello vistula, in my first spring controller";}
 
 
-        @GetMapping( "/greeting")
-        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-            model.addAttribute("name", "name");
-            return "greeting";
-        }
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
      //http://localhost:8080/greeting?name=Vistula
     }
 
